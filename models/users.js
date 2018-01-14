756c0da4dc7fcef74e760ee60090413065f6c28e
Jilvin <jilvinjacob@gmail.com>
2018-01-14 21:30:57 +0530

    minor change in users model

--- models/users.js
+++ models/users.js
@@ -46,11 +46,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(48),
       unique: false,
       allowNull: false
-    },
-    channel: {
-      type: DataTypes.INTEGER,
-      unique: false,
-      allowNull: false
     }
   }, {
     classMethods: {
