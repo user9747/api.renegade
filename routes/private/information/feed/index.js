2a6fdec6d494145f1c6161fee2346045356d2013
Jilvin <jilvinjacob@gmail.com>
2018-01-17 18:11:24 +0530

    correction in private feeds api

--- routes/private/information/feed/index.js
+++ routes/private/information/feed/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var bearerToken = require('express-bearer-token');
 
+var _ = require('lodash');
+var Promise = require("bluebird");
+
 var JWTsecret = require('../../../../config/config.js').JWTsecret;
 var JWTaudience = require('../../../../config/config.js').JWTaudience;
 var JWTissuer = require('../../../../config/config.js').JWTissuer;
@@ -76,7 +79,8 @@ router.post('/', bearerToken(), function(req, res, next) {
         models.post_data.findAll({
           offset: offset,
           limit: required_number,
-          order: [['updatedAt', 'DESC']]
+          order: [['updatedAt', 'DESC']],
+          raw: true
         }).then(function(result){
           if(result == null || result.length == 0)
           {
@@ -87,32 +91,87 @@ router.post('/', bearerToken(), function(req, res, next) {
           }
           else
           {
-            // result = result[0].data;
+            var returnObject = [];
+            var promises = [];
 
-            // return result;
-            res.json({ "state": "success",
-            "description_slug": "success-feeds",
-            "description": "Feeds obtained successfully.",
-            "data": result });
-          }
-        }).catch(function (err) {
-          console.log(err);
-          // handle error;
-          res.status(500).json({ "success": "false" });
+            function asyncPostElementProcess(post)
+            {
+              return new Promise(function(resolve){
+                models.users.findAll({
+                  where: { id: post.author_id },
+                  attributes: ['username', 'first_name', 'second_name'],
+                  raw: true
+                }).then(function(user){
+                  if(user == null || user.length == 0)
+                  {
+                    // such a user does not exist
+                    res.status(500).json({ "state": "failure",
+                    "description_slug": "error-unknown",
+                    "description": "Unknown error encountered internally."
+                  });
+
+                  reject();
+                }
+                else
+                {
+                  // user exists
+                  var returnElement = post;
+
+                  delete returnElement.author_id;
+
+                  returnElement.author = user;
+
+                  returnObject.push(returnElement);
+
+                  resolve();
+                }
+              }).catch(function (err) {
+                console.log(err);
+                // handle error;
+                res.status(500).json({ "state": "failure",
+                "description_slug": "error-unknown",
+                "description": "Unknown error encountered internally."
+              });
+
+              reject();
+            });
+          });
+        }
+
+        _.forEach(result, function(post) {
+          promises.push(asyncPostElementProcess(post));
         });
-      }
-    }
-    else
-    {
-      // log error to the console
-      console.log(err);
 
-      // error occurred
-      res.json({"state": "failure",
-      "description-slug": "error-jwt-verification",
-      "description": "Error occurred during verification of jwt token. Verify token used or obtain new token from Authentication API."
-    });
-  }
+        Promise.all(promises).then(function() {
+          // return result;
+          res.json({ "state": "success",
+          "description_slug": "success-feeds",
+          "description": "Feeds obtained successfully.",
+          "data": returnObject
+        });
+      });
+    }
+  }).catch(function (err) {
+    console.log(err);
+    // handle error;
+    res.status(500).json({ "state": "failure",
+    "description_slug": "error-unknown",
+    "description": "Unknown error encountered internally."
+  });
+});
+}
+}
+else
+{
+  // log error to the console
+  console.log(err);
+
+  // error occurred
+  res.json({"state": "failure",
+  "description-slug": "error-jwt-verification",
+  "description": "Error occurred during verification of jwt token. Verify token used or obtain new token from Authentication API."
+});
+}
 });
 });
 
