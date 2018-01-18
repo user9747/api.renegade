a80a2012b121e8b8650187840cd1ba1014481d41
Jilvin <jilvinjacob@gmail.com>
2018-01-18 19:14:33 +0530

    added public information field field_id sub_fields api

--- routes/public/information/index.js
+++ routes/public/information/index.js
@@ -43,4 +43,72 @@ router.get('/occupations', function(req, res, next) {
 })
 });
 
+router.get('/field/:field_id/sub_fields', function(req, res, next) {
+
+  field_id = parseInt(req.params.field_id);
+
+  if(!field_id)
+  {
+    // array empty
+    res.status(400).json({
+      "state": "failure",
+      "description-slug": "error-field_id-parameter-invalid-url",
+      "description": "field_id parameter missing or invalid in url. check url."
+    });
+  }
+  else
+  {
+    // check if valid field_id
+    models.fields.findOne({
+      where: { id: field_id }
+    }).then(function(field){
+      if(field == null)
+      {
+        // no field exist with the give field_id
+        res.json({
+          "state": "failure",
+          "description-slug": "error-field_id-invalid",
+          "description": "Requested field is not available in the system."
+        });
+      }
+      else
+      {
+        models.sub_fields.findAll({
+          include: [{ model: models.fields, where: { id: field_id }, attributes: []}],
+          attributes: ['id', 'sub_field_name']
+        }).then(function(sub_fields) {
+          // fields will be an array of all fields instances
+          if(sub_fields.length == 0)
+          {
+            // array empty
+            res.json({
+              "state": "failure",
+              "description-slug": "error-sub_fields-empty",
+              "description": "sub_fields empty for field_id: "+field_id
+            });
+          }
+          else
+          {
+            res.json(sub_fields);
+          }
+        }).catch(function(err){
+          // array empty
+          res.status(500).json({
+            "state": "failure",
+            "description-slug": "error-field-sub_fields",
+            "description": "Unable to complete request."
+          });
+        });
+      }
+    }).catch(function(err){
+      // array empty
+      res.status(500).json({
+        "state": "failure",
+        "description-slug": "error-field-sub_fields",
+        "description": "Unable to complete request."
+      });
+    });
+  }
+});
+
 module.exports = router;
