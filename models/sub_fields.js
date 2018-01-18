b7ea0c6e8ba7da2c2020b1cc4dff02b3685321b2
Jilvin <jilvinjacob@gmail.com>
2018-01-18 18:16:37 +0530

    added sub_field_add to request body for private create post api

--- models/sub_fields.js
+++ models/sub_fields.js
@@ -11,8 +11,23 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(100),
       unique: false,
       allowNull: false
+    },
+    field_id: {
+      type: DataTypes.INTEGER,
+      unique: false,
+      allowNull: false
     }
   });
+
+  Sub_Fields.associate = function(models) {
+    models.sub_fields.belongsTo(models.fields, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        name: "field_id"
+        // allowNull: false -- already defined
+      }
+    });
+  };
 
   return Sub_Fields;
 }
--- routes/private/create/post/index.js
+++ routes/private/create/post/index.js
@@ -12,6 +12,9 @@ var models = require('../../../../models');
 
 var sequelize = models.sequelize;
 
+var Promise = require("bluebird");
+var _ = require("lodash");
+
 router.get('/', function(req, res, next) {
   res.json({"status": "functional"});
 });
@@ -36,16 +39,80 @@ router.post('/', bearerToken(), function(req, res, next) {
 
           // chain all your queries here. make sure you return them.
           return models.post_owner_association.create({
-            owner_id: userID
-          }, {transaction: t}).then(function (association) {
+            owner_id: userID,
+            post_status: "v"
+          }, {transaction: t}).then(function (post_owner_association) {
+            // save in variables
+            var post_id = post_owner_association.id;
+            var post_content = req.body.post_content;
+            var post_text = req.body.post_text;
+            var post_image = req.body.post_image;
+
             return models.post_data.create({
-              post_id: association.id,
+              post_id: post_id,
               author_id: userID,
-              post_content: req.body.post_content,
-              post_status: "v",
-              post_text: req.body.post_text,
-              post_image: req.body.post_image
-            }, {transaction: t});
+              post_content: post_content,
+              post_element_status: "v",
+              post_text: post_text,
+              post_image: post_image
+            }, {transaction: t}).then(function(post_data_element) {
+
+              var add = req.body.sub_field_add;
+
+              if(!add || add.length == 0)
+              {
+                throw new Error("sub_field_add missing in request!");
+              }
+              else
+              {
+                var promisesAdd = [];
+
+                function asyncAddSubFieldAssociation(sub_field_id)
+                {
+                  return models.post_sub_field_association.findAndCountAll({
+                    where: {
+                      post_id: post_id,
+                      sub_field_id: sub_field_id,
+                      association_status: "v"
+                    }
+                  }, {transaction: t}).then(function(post_sub_field_association){
+                    if(post_sub_field_association.count == 0 || post_sub_field_association.count == 1)
+                    {
+                      // valid data in database
+                      if(post_sub_field_association.count == 0)
+                      {
+                        // valid association does not exist in database
+
+                        // create valid association
+                        return models.post_sub_field_association.create({
+                          post_id: post_id,
+                          sub_field_id: sub_field_id,
+                          association_status: "v"
+                        }, {transaction: t});
+                      }
+                      else
+                      {
+                        // valid association exist in database
+                        throw new Error("Valid association already exists in database. post_id: "+post_id+" sub_field_id: "+sub_field_id+" rows: "+post_sub_field_association.rows);
+                      }
+                    }
+                    else
+                    {
+                      // invalid data in database
+                      throw new Error("Invalid data found in database. Please rectify dataset ASAP. post_id: "+post_id+" sub_field_id: "+sub_field_id+" rows: "+post_sub_field_association.rows);
+                    }
+                  });
+                }
+
+                _.forEach(add, function(value) {
+                  // console.log(value);
+                  // console.log(addStatusArray);
+                  promisesAdd.push(asyncAddSubFieldAssociation(value));
+                });
+
+                return Promise.all(promisesAdd);
+              }
+            });
           });
 
         }).then(function (result) {
@@ -55,13 +122,16 @@ router.post('/', bearerToken(), function(req, res, next) {
           "description_slug": "success-post-create",
           "description": "Post successfully created."});
         }).catch(function (err) {
-          console.log(err);
-
           // Transaction has been rolled back
           // err is whatever rejected the promise chain returned to the transaction callback
-          res.status(500).json({"state": "failure",
-          "description_slug": "failure-post-create",
-          "description": "Post could not be created."});
+
+          console.log(err);
+
+          res.status(500).json({
+            "state": "failure",
+            "description_slug": "failure-post-create",
+            "description": "Post could not be created."
+          });
         });
       }
     }
@@ -71,12 +141,13 @@ router.post('/', bearerToken(), function(req, res, next) {
       console.log(err);
 
       // error occurred
-      res.json({"state": "failure",
-      "description-slug": "error-jwt-verification",
-      "description": "Error occurred during verification of jwt token. Verify token used or obtain new token from Authentication API."
-    });
-  }
-});
+      res.json({
+        "state": "failure",
+        "description-slug": "error-jwt-verification",
+        "description": "Error occurred during verification of jwt token. Verify token used or obtain new token from Authentication API."
+      });
+    }
+  });
 });
 
 module.exports = router;
