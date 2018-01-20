8b372eb4433d80bf90170ab830b996f3b4a47e7d
Mohd-Bilal <bilaljaleel123@gmail.com>
2018-01-20 19:32:25 +0530

    bug

--- routes/private/update/profile/personal/index.js
+++ routes/private/update/profile/personal/index.js
@@ -4,13 +4,11 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var bearerToken = require('express-bearer-token');
 
-var _ = require('lodash');
+var JWTsecret = require('../../../../../config/config.js').JWTsecret;
+var JWTaudience = require('../../../../../config/config.js').JWTaudience;
+var JWTissuer = require('../../../../../config/config.js').JWTissuer;
+
+var models = require('../../../../../models');
+
 var Promise = require("bluebird");
-
-var JWTsecret = require('../../../../config/config.js').JWTsecret;
-var JWTaudience = require('../../../../config/config.js').JWTaudience;
-var JWTissuer = require('../../../../config/config.js').JWTissuer;
-
-var models = require('../../../../models');
-
-var sequelize = models.sequelize;
+var _ = require("lodash");
